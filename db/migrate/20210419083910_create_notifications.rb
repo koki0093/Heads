4c6f86274641a22c762620e6062fa797f7c4ef1d
Owner <owner@OwnernoMacBook-puro.local>
2021-05-01 19:32:32 +0900

    Release to heroku

--- db/migrate/20210419083910_create_notifications.rb
+++ db/migrate/20210419083910_create_notifications.rb
@@ -4,19 +4,14 @@ class CreateNotifications < ActiveRecord::Migration[6.1]
       t.integer :visitor_id, null: false
       t.integer :visited_id, null: false
       t.integer :snkr_id
-      t.integer :message_id
       t.integer :offer_id
       t.integer :room_id
       t.string :action, default: '', null: false
       t.boolean :checked, default: false, null: false
-
-
-
       t.timestamps
     end
     add_index :notifications, :visitor_id
     add_index :notifications, :visited_id
     add_index :notifications, :snkr_id
-    add_index :notifications, :massage_id
   end
 end
